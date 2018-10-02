7f33b3bc20534bc0370ba9975d6598655f426fdf
Tore Wentzel-Larsen <42468973+ToreWentzel-Larsen@users.noreply.github.com>
2018-10-02 21:34:09 +0200

    Update empmed.r

--- empmed.r
+++ empmed.r
@@ -99,3 +99,67 @@ cl14
 cl15
 
 # check with simulations:
+simclem <- function(m1=14, nsim = 1e+05) {
+  n1 <- 2*m1
+  cs <- rep(NA, nsim)
+  ls <- rep(NA, nsim)
+  for (sim in 1:nsim) {
+    series <- rnorm(n1)
+    med <- median(series)
+    above <- as.numeric(series>med)
+    rleabove <- rle(above)$lengths
+    cs[sim] <- length(rleabove) - 1
+    ls[sim] <- max(rleabove)
+  } # end for sim
+  return(data.frame(cs=cs,ls=ls))
+} #end function simclem
+# auxiliary funtions:
+cumsummrow <- function(mtrx) {
+  mtrs <- mtrx
+  nrw <- nrow(mtrx)
+  for (rw in 1:nrw) mtrs[rw, ] <- cumsum(mtrx[rw, ])
+  return(mtrs)
+} # end function cumsumrow
+cumsummcol <- function(mtrx) {
+  mtrs <- mtrx
+  ncl <- ncol(mtrx)
+  for (cl in 1:ncl) mtrs[, cl] <- cumsum(mtrx[, cl])
+  return(mtrs)
+} # end function cumsummcol
+
+#simulate for m=14 (n=2*14=28):
+simcl14 <- simclem(m1=14)
+# check means of C and L:
+sum(apply(cl14,1,sum)*c(1:27))/sum(cl14)
+mean(simcl14$cs)
+sum(apply(cl14,2,sum)*c(1:14))/sum(cl14)
+mean(simcl14$ls)
+# check standard deviations of C and L:
+sqrt(sum(apply(cl14,1,sum)*c(1:27)^2)/sum(cl14) - 
+       (sum(apply(cl14,1,sum)*c(1:27))/sum(cl14))^2)
+sd(simcl14$cs)
+sqrt(sum(apply(cl14,2,sum)*c(1:14)^2)/sum(cl14) - 
+       (sum(apply(cl14,2,sum)*c(1:14))/sum(cl14))^2)
+sd(simcl14$ls)
+# check means of C*L:
+sum(diag(1:27) %*% cl14 %*% diag(1:14))/sum(cl14)
+mean(simcl14$cs*simcl14$ls)
+# check theoretical and empirical cdf:
+plot(x=as.numeric(names(table(simcl14$cs))),
+     y=(cumsum(cumsummrow(cl14)[,14])/(sum(cl14)))[
+       as.numeric(names(table(simcl14$cs)))], 
+     type="l", xlab="Number of crossings", ylab="CDF", las=1)
+points(x=as.numeric(names(table(simcl14$cs))), 
+       y=cumsum(table(simcl14$cs))/sum(table(simcl14$cs)),
+       type="l", col="red",lty="dotted")
+lines(x=c(4,7), y=c(.9,.9), col="red")
+text(x=7, y=.9, pos=4, labels="red: simulations", col="red")
+plot(x=as.numeric(names(table(simcl14$ls))),
+     y=(cumsum(cumsummcol(cl14)[27,])/(sum(cl14)))[
+       as.numeric(names(table(simcl14$ls)))], 
+     type="l", xlab="Longest run", ylab="CDF", las=1)
+points(x=as.numeric(names(table(simcl14$ls))), 
+       y=cumsum(table(simcl14$ls))/sum(table(simcl14$ls)),
+       type="l", col="red",lty="dotted")
+lines(x=c(4,6), y=c(.4,.4), col="red")
+text(x=6, y=.4, pos=4, labels="red: simulations", col="red")
