63ec5f861ebd7163cf77ff5922d28f9c691162ea
Tore Wentzel-Larsen <42468973+ToreWentzel-Larsen@users.noreply.github.com>
2018-10-05 15:19:05 +0200

    Update empmed.r

--- empmed.r
+++ empmed.r
@@ -1,3 +1,5 @@
+vlibrary(Rmpfr)
+
 # finction for generating all subsets of size m1 within n1 objects:
 gensubset <- function(n1, m1) {  
   chooseall <- choose(n1,m1)
@@ -161,3 +163,92 @@ points(x=as.numeric(names(table(simcl14$ls))),
        type="l", col="red",lty="dotted")
 lines(x=c(4,6), y=c(.4,.4), col="red")
 text(x=6, y=.4, pos=4, labels="red: simulations", col="red")
+
+
+
+
+# function for computing the number of subsets of each size m
+# from n subsequent observations, for each value of the number of
+# crossings and the longest run. subsets are also classified by
+# whether they contain the first observation and the computations
+# partition on the length of the initial run.
+crossrunem <- function(nmax = 100, prec = 120,
+                        printn = FALSE) {
+  nill <- Rmpfr::mpfr(0, prec)
+  one <- Rmpfr::mpfr(1, prec)
+  # conditioning of S= first value, pat: above 0, pbt: below 0 suffix
+  # t: probabilities times multm^(n-1).  n=1:
+  nat <- list(pt1 = Rmpfr::mpfr2array(one, dim = c(1, 1)))
+  pbt <- list(pt1 = Rmpfr::mpfr2array(one, dim = c(1, 1)))
+  pt <- list(pt1 = Rmpfr::mpfr2array(one, dim = c(1, 1)))
+  qat <- list(pt1 = Rmpfr::mpfr2array(one, dim = c(1, 1)))
+  qbt <- list(pt1 = Rmpfr::mpfr2array(one, dim = c(1, 1)))
+  qt <- list(pt1 = Rmpfr::mpfr2array(one, dim = c(1, 1)))
+  for (nn in 2:nmax) {
+    pat[[nn]] <- Rmpfr::mpfr2array(rep(nill, nn * nn), dim = c(nn, nn))
+    pbt[[nn]] <- Rmpfr::mpfr2array(rep(nill, nn * nn), dim = c(nn, nn))
+    rownames(pat[[nn]]) <- c(0:(nn - 1))
+    rownames(pbt[[nn]]) <- c(0:(nn - 1))
+    colnames(pat[[nn]]) <- c(1:nn)
+    colnames(pbt[[nn]]) <- c(1:nn)
+    pat[[nn]][1, nn] <- (pmultm^(nn - 1))  # from cond on no crossing
+    pbt[[nn]][1, nn] <- (qmultm^(nn - 1))  # from cond on no crossing
+    for (ff in 2:nn) {
+      # from cond on first crossing at ff if last part shortest:
+      if (nn - ff + 1 <= ff - 1)
+      {
+        f1 <- ff  # unnecessary, but makes code checking easier
+        pat[[nn]][2:(nn - f1 + 2), f1 - 1] <- pat[[nn]][2:(nn -
+                                                             f1 + 2), f1 - 1] + (pmultm^(f1 - 2)) * qmultm *
+          qbt[[nn - f1 + 1]][1:(nn - f1 + 1), nn - f1 + 1]
+        pbt[[nn]][2:(nn - f1 + 2), f1 - 1] <- pbt[[nn]][2:(nn -
+                                                             f1 + 2), f1 - 1] + (qmultm^(f1 - 2)) * pmultm *
+          qat[[nn - f1 + 1]][1:(nn - f1 + 1), nn - f1 + 1]
+      }  # end if last part shortest
+      if (nn - ff + 1 > ff - 1)
+      {
+        # if last part longest
+        f2 <- ff  # unnecessary, but makes code checking easier
+        pat[[nn]][2:(nn - f2 + 2), f2 - 1] <- pat[[nn]][2:(nn -
+                                                             f2 + 2), f2 - 1] + (pmultm^(f2 - 2)) * qmultm *
+          qbt[[nn - f2 + 1]][1:(nn - f2 + 1), f2 - 1]
+        pat[[nn]][2:(nn - f2 + 2), f2:(nn - f2 + 1)] <- pat[[nn]][2:(nn -
+                                                                       f2 + 2), f2:(nn - f2 + 1)] + (pmultm^(f2 - 2)) *
+          qmultm * pbt[[nn - f2 + 1]][1:(nn - f2 + 1), f2:(nn -
+                                                             f2 + 1)]
+        pbt[[nn]][2:(nn - f2 + 2), f2 - 1] <- pbt[[nn]][2:(nn -
+                                                             f2 + 2), f2 - 1] + (qmultm^(f2 - 2)) * pmultm *
+          qat[[nn - f2 + 1]][1:(nn - f2 + 1), f2 - 1]
+        pbt[[nn]][2:(nn - f2 + 2), f2:(nn - f2 + 1)] <- pbt[[nn]][2:(nn -
+                                                                       f2 + 2), f2:(nn - f2 + 1)] + (qmultm^(f2 - 2)) *
+          pmultm * pat[[nn - f2 + 1]][1:(nn - f2 + 1), f2:(nn -
+                                                             f2 + 1)]
+      }  # end if last part longest
+    }  # end for ff
+    pt[[nn]] <- pm * pat[[nn]] + qm * pbt[[nn]]
+    qat[[nn]] <- cumsumm(pat[[nn]])
+    qbt[[nn]] <- cumsumm(pbt[[nn]])
+    qt[[nn]] <- pm * qat[[nn]] + qm * qbt[[nn]]
+    rownames(pt[[nn]]) <- c(0:(nn - 1))
+    colnames(pt[[nn]]) <- c(1:nn)
+    rownames(qat[[nn]]) <- c(0:(nn - 1))
+    colnames(qat[[nn]]) <- c(1:nn)
+    rownames(qbt[[nn]]) <- c(0:(nn - 1))
+    rownames(qat[[nn]]) <- c(0:(nn - 1))
+    colnames(qt[[nn]]) <- c(1:nn)
+    colnames(qt[[nn]]) <- c(1:nn)
+    if (printn)
+    {
+      print(nn)
+      print(Sys.time())
+    }  # end optional timing information
+  }  # end for nn
+  names(pat) <- paste("pat", 1:nmax, sep = "")
+  names(pbt) <- paste("pbt", 1:nmax, sep = "")
+  names(pt) <- paste("pt", 1:nmax, sep = "")
+  names(qat) <- paste("qat", 1:nmax, sep = "")
+  names(qbt) <- paste("qbt", 1:nmax, sep = "")
+  names(qt) <- paste("qt", 1:nmax, sep = "")
+  return(list(pat = pat, pbt = pbt, pt = pt, qat = qat, qbt = qbt,
+              qt = qt))
+} # end function cl
